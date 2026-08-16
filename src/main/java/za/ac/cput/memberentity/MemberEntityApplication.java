4a5c9dd01140654dae44866eb4e39a46ec46d86c
Stephanie Lewu <230211216@mycput.ac.za>
2026-08-16 23:59:49 +0200

    feat: payment & category controllers & services updated

---
+++ src/main/java/za/ac/cput/memberentity/MemberEntityApplication.java
@@ -0,0 +1,13 @@
+package za.ac.cput.memberentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberEntityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/za/ac/cput/memberentity/MemberEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package za.ac.cput.memberentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberEntityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
